25c0ff7847b2eed3dc767512d30882d3e43c98aa
Bryce <bryce.christian@gmail.com>
2025-04-06 01:28:56 -0400

    Remove redundant schema import

--- docs/main.tsx
+++ docs/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { schema } from './schema';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
